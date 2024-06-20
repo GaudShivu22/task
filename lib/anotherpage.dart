2ae07c60090a879a1eb04c8cddc80971e7d78366
GaudShivu22 <142712013+GaudShivu22@users.noreply.github.com>
2024-06-20 11:59:09 +0530

    Update anotherpage.dart

--- lib/anotherpage.dart
+++ lib/anotherpage.dart
@@ -1,55 +1,33 @@
 import 'package:flutter/material.dart';
 
-class AnotherPage extends StatefulWidget {
+class AnotherPage extends StatelessWidget {
   final String currentAuthor;
   final String currentQuote;
   final Future<void> Function() fetchApiResponse;
 
-  const AnotherPage({
-    Key? key,
-    required this.currentAuthor,
-    required this.currentQuote,
-    required this.fetchApiResponse,
-  }) : super(key: key);
+  AnotherPage({required this.currentAuthor, required this.currentQuote, required this.fetchApiResponse});
 
-  @override
-  State<AnotherPage> createState() => _AnotherPageState();
-}
-
-class _AnotherPageState extends State<AnotherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Another Page'),
+        title: Text('Quote Page'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
+      body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(height: 60),
-
-         //  call fetchapi function here
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
             Center(
               child: ElevatedButton(
                 onPressed: () async {
-                  await widget.fetchApiResponse();
+                  await fetchApiResponse();
                 },
-                child: const Text("Fetch New Quote"),
+                child: Text('Fetch New Quote'),
               ),
             ),
-            const Center(
-              child: Text(
-                'Quote of the day',
-                style: TextStyle(fontSize: 16),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Text(
-              ' "${widget.currentQuote}" : ${widget.currentAuthor} ',
-              style: const TextStyle(fontSize: 16),
-            ),
+            Text('Quote: $currentQuote'),
+            Text('Author: $currentAuthor'),
+
 
           ],
         ),
