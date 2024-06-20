754bb8fd1b2980d39f7d39ea283f01c0af1737ec
GaudShivu22 <142712013+GaudShivu22@users.noreply.github.com>
2024-06-20 08:56:22 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:task/anotherpage.dart';
+import 'anotherpage.dart';
 import 'apiendpoint.dart';
 import 'diocilent.dart';
+import 'form_validation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,39 +29,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class MyHomePageState extends State<MyHomePage> {
-
   var num1 = 0;
   var num2 = 0;
   var result = 0;
   var author = '';
   var quote = '';
 
+  final Key = GlobalKey<FormState>();
 
   void addNumbers() {
-    setState(() {
-      result = num1 + num2;
-    });
+    if (Key.currentState!.validate()) {
+      setState(() {
+        result = num1 + num2;
+        addToHistory();
+        fetchApiResponse();
+      });
+    }
   }
 
-
   List<String> calculationHistory = [];
+
   void addToHistory() {
     setState(() {
       calculationHistory.add("$num1 + $num2 = $result");
     });
   }
 
-Future<void> fetchApiResponse() async {
+  Future<void> fetchApiResponse() async {
     try {
       final response = await DioClient.dioClient.getAPI(endPoint: ApiEndPoint.getQuotes);
-      // updateQuote(response.data[0]['a'].toString(), response.data[0]['q'].toString());
       final newAuthor = response.data[0]['a'].toString();
       final newQuote = response.data[0]['q'].toString();
       updateQuote(newAuthor, newQuote);
-       } catch (e) {
+    } catch (e) {
       print('Error fetching API: $e');
       updateQuote('', 'Error fetching quote');
-
     }
   }
 
@@ -72,10 +75,22 @@ Future<void> fetchApiResponse() async {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => AnotherPage(author: newAuthor, quote: newQuote),
+        builder: (context) => AnotherPage(
+          currentAuthor: author,
+          currentQuote: quote,
+          fetchApiResponse: fetchApiResponse,
+        ),
       ),
     );
   }
+
+
+
+
+
+
+
+
 
 
 
@@ -85,85 +100,89 @@ Future<void> fetchApiResponse() async {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(height: 60),
-
-            const Center(child: Text("Adder",style: TextStyle(fontSize: 40,fontWeight: FontWeight.w600),)),
-            Row(
-              children: <Widget>[
-                Flexible(
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                        labelText: 'Enter number 1',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.zero,
-                    ),),
-                    onChanged: (value) {
-                      num1 = int.tryParse(value)?? 0;
-                    },
-                  ),
+        child: Form(
+          key: Key,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 60),
+              const Center(
+                child: Text(
+                  "Adder",
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.w600),
                 ),
-                const Icon(Icons.add),
-                Flexible(
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
+              ),
+              Row(
+                children: <Widget>[
+                  Flexible(
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: 'Enter number 1',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.zero,
+                        ),
+                      ),
+                      validator: validateNumericalInput,
+                      onChanged: (value) {
+                        num1 = int.tryParse(value) ?? 0;
+                      },
+                    ),
+                  ),
+                  const Icon(Icons.add),
+                  Flexible(
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
                         labelText: 'Enter number 2',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.zero,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.zero,
+                        ),
+                      ),
+                      validator: validateNumericalInput,
+                      onChanged: (value) {
+                        num2 = int.tryParse(value) ?? 0;
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  GestureDetector(
+                    onTap: addNumbers,
+                    child: const Text(
+                      "=",
+                      style: TextStyle(fontWeight: FontWeight.w600, fontSize: 35),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Flexible(
+                    child: TextFormField(
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        labelText: "$result",
                       ),
                     ),
-                    onChanged: (value) {
-                      num2 = int.tryParse(value) ?? 0;
-                    },
                   ),
-                ),
-
-                const SizedBox(width: 10,),
-
-                GestureDetector(
-                  onTap: () {
-                    addNumbers();
-                    addToHistory();
-                    fetchApiResponse();},
-                  child: const Text("=",style: TextStyle(fontWeight: FontWeight.w600,fontSize:35),),
-                ),
-
-                const SizedBox(width: 10),
-                Flexible(
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        labelText: "$result"
-                    ),
-
-                  ),
-                ),
-              ],
-            ),
-
-            const SizedBox(height: 40),
-            const Center(child: Text("History",style: TextStyle(fontWeight: FontWeight.w400,fontSize:20),)),
-            Expanded(
-              child: ListView.builder(
-                itemCount: calculationHistory.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                      title: Text(".${calculationHistory[index]}")
-                  );
-                },
+                ],
               ),
-            ),
-
-
-
-          ],
+              const SizedBox(height: 40),
+              const Center(
+                child: Text(
+                  "History",
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: calculationHistory.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(title: Text(".${calculationHistory[index]}"));
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
