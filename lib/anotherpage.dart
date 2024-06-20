e0460cbba082bc72368b05d0e2bb79cab44878b0
GaudShivu22 <142712013+GaudShivu22@users.noreply.github.com>
2024-06-20 13:19:24 +0530

    Update anotherpage.dart

--- lib/anotherpage.dart
+++ lib/anotherpage.dart
@@ -1,40 +1,58 @@
 import 'package:flutter/material.dart';
 
 class AnotherPage extends StatefulWidget {
-  final String currentAuthor;
-  final String currentQuote;
-  final Future<void> Function() fetchApiResponse;
+  const AnotherPage({super.key, required this.fetchApi,required this.Quote});
+  final Future<String> Function() fetchApi;
+   final String Quote;
 
-  AnotherPage({required this.currentAuthor, required this.currentQuote, required this.fetchApiResponse});
 
   @override
   State<AnotherPage> createState() => _AnotherPageState();
 }
 
 class _AnotherPageState extends State<AnotherPage> {
+ late String quote;
+
+
+  @override
+  void initState() {
+    super.initState();
+    quote = widget.Quote;
+
+  }
+
+ Future<void> newquote() async {
+   String newQuote = await widget.fetchApi();
+   setState(() {
+     quote = newQuote;
+   });
+ }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Quote Page'),
+        title: Text('Another Page'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: ElevatedButton(
-                onPressed: () async {
-                  await widget.fetchApiResponse();
-                },
-                child: Text('Fetch New Quote'),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: newquote,
+                child: Text('Update Quote'),
               ),
-            ),
-            Text('Quote: ${widget.currentQuote}'),
-            Text('Author: ${widget.currentAuthor}'),
+              Text(
+                quote,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
 
-
-          ],
+            ],
+          ),
         ),
       ),
     );
